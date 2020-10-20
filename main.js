8d76df690396af599d9a754b863fc76c365c418f
Jakub Wierzchowski <wierzcho155@gmail.com>
2020-10-20 11:53:46 +0200

    .

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
 const name = "Maciek";
 const age = 32;
-console.log(name);
-console.log("hej");
-alert(ok);
+
+console.log(
+    `Witaj Drogi Odwiedzający, nazywam się ${firstName} i mam ${age} lata.`
+);
+const heading = document.querySelector('.main-heading--js');
+
+heading.innerHTML = `Witaj Drogi Odwiedzjący, nazywam się ${firstName} i mam ${age} lata.`
+
+const emptyParagraph = document.querySelector('.header-two__description--js');
+emptyParagraph.innerHTML = `Zmiana treści przez js`;
